836c715a1c1240563385c8690fe2471f3a58fb11
LuK1337 <priv.luk@gmail.com>
2021-08-23 18:00:54 +0200

    sm8150-common: RIP `remove_packages`
    No longer necessary since now we can just pass `OVERRIDES` to extract
    utils and it'll handle it automatically for us :^)
    
    Change-Id: I19c97f5daa72d809dae2442fcb2b8b81a702aaf2
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -387,10 +387,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oneplus
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
