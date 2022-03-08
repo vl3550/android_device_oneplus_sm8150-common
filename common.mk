78a5de5e554a3b7f462b68f6cf378f8397feaf5f
Vaisakh Murali <mvaisakh@statixos.com>
2022-03-08 15:00:22 +0530

    sm8150-common: Build needed vendor HIDL interface for sensors HAL impl
    /vendor/lib64/sensors.ssc.so depends on android.hardware.audio@4.0.so.
    
    Change-Id: Ib8d75e223c0db5267bd93021f172e412fb9d73c7

--- common.mk
+++ common.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
+    android.hardware.audio@4.0.vendor \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
